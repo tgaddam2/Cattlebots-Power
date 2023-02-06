09117c4aee69beabd7307c7e3d50f95ea0396317
tgaddam2 <taranggaddam@gmail.com>
2023-02-06 17:14:42 -0500

    roadruner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraBlueOrange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraBlueOrange.java
@@ -18,7 +18,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
